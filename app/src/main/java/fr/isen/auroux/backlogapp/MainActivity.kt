e142157bc0f5cdde4eac99445be2c786f6a92d05
axel-auroux <axel.auroux@isen.yncrea.fr>
2021-02-09 17:02:30 +0100

    init firebase

---
+++ app/src/main/java/fr/isen/auroux/backlogapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package fr.isen.auroux.backlogapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import fr.isen.auroux.backlogapp.databinding.ActivityMainBinding
+import fr.isen.auroux.backlogapp.authentication.SignInActivity
+import fr.isen.auroux.backlogapp.authentication.SignUpActivity
+
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+
+        }
+    }
+}
---
+++ app/src/main/java/fr/isen/auroux/backlogapp/network/User.kt
@@ -0,0 +1,9 @@
+package fr.isen.mahdi.backlogapp.network
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class User(val username: String? = null, val email: String? = null, val  lastname: String? = null, val firstname: String? = null, val id: String? =null) {
+    // Null default values create a no-argument default constructor, which is needed
+    // for deserialization from a DataSnapshot.
+}
