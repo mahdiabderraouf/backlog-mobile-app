8be80761bd8df8f4a01f125fea5294d65232345e
axel-auroux <axel.auroux@isen.yncrea.fr>
2021-02-20 18:30:29 +0100

    update 20/02

--- app/src/main/java/fr/isen/auroux/backlogapp/projects/ProjectsActivity.kt
+++ app/src/main/java/fr/isen/auroux/backlogapp/projects/ProjectsActivity.kt
@@ -3,6 +3,7 @@ package fr.isen.auroux.backlogapp.projects
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.CheckBox
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -14,18 +15,21 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import fr.isen.auroux.backlogapp.BaseActivity
+import fr.isen.auroux.backlogapp.R
 import fr.isen.auroux.backlogapp.databinding.ActivityProjectdisplayBinding
 import fr.isen.auroux.backlogapp.network.Project
+
 
 class ProjectsActivity : BaseActivity(), PostCellClickListener {
     private lateinit var binding: ActivityProjectdisplayBinding
     private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
+    lateinit var finished: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProjectdisplayBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         database = Firebase.database("").reference
         auth = Firebase.auth
@@ -35,6 +39,12 @@ class ProjectsActivity : BaseActivity(), PostCellClickListener {
         binding.addProject.setOnClickListener {
             val intent = Intent(this, AddProjectActivity::class.java)
             startActivity(intent)
+        }
+
+        val finished = findViewById(R.id.checkBox) as CheckBox
+        val result = StringBuilder()
+        if (finished.isChecked) {
+                result.append("project finished/n")
         }
     }
 
