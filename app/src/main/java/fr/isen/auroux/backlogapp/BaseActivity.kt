f26386acb7f280a95767fd53f9e7f1b94a43bd40
mahdiabderraouf <mahdi.abderraouf@outlook.com>
2021-03-11 10:45:19 +0100

    Drag and drop works

--- app/src/main/java/fr/isen/auroux/backlogapp/BaseActivity.kt
+++ app/src/main/java/fr/isen/auroux/backlogapp/BaseActivity.kt
@@ -2,20 +2,27 @@ package fr.isen.auroux.backlogapp
 
 import android.content.Intent
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
-        val menuView = menu.findItem(R.id.logout).actionView
-
-        menuView.setOnClickListener {
-            Firebase.auth.signOut()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.logout -> {
+                Firebase.auth.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/fr/isen/auroux/backlogapp/task/TaskAdapter.kt
+++ app/src/main/java/fr/isen/auroux/backlogapp/task/TaskAdapter.kt
@@ -3,14 +3,12 @@ package fr.isen.auroux.backlogapp.task
 import android.content.ClipData
 import android.content.ClipDescription
 import android.content.Context
-import android.graphics.Color
 import android.os.Build
 import android.util.Log
 import android.view.DragEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -19,11 +17,10 @@ import androidx.recyclerview.widget.RecyclerView
 import fr.isen.auroux.backlogapp.databinding.TaskCellBinding
 import fr.isen.auroux.backlogapp.network.Task
 
-class TaskAdapter (
+class TaskAdapter(
     private val tasks: List<Task>,
     private val context: Context
-    ): RecyclerView.Adapter<TaskAdapter.TasksViewHolder>()
-{
+) : RecyclerView.Adapter<TaskAdapter.TasksViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
         return TasksViewHolder(
             TaskCellBinding.inflate(
@@ -37,23 +34,23 @@ class TaskAdapter (
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
         holder.textView.text = tasks[position].title
-        holder.cardView.setOnLongClickListener { v: View ->
-            // Create a new ClipData.
-            // This is done in two steps to provide clarity. The convenience method
-            // ClipData.newPlainText() can create a plain text ClipData in one step.
+        holder.cardView.setOnLongClickListener(tasks[position].id?.let { onLongClickListener(it) })
+        holder.cardView.setOnDragListener(onDragListener())
+    }
 
-            // Create a new ClipData.Item from the ImageView object's tag
-            val item = ClipData.Item(v.tag as? CharSequence)
+    override fun getItemCount(): Int {
+        return tasks.count()
+    }
 
-            // Create a new ClipData using the tag as a label, the plain text MIME type, and
-            // the already-created item. This will create a new ClipDescription object within the
-            // ClipData, and set its MIME type entry to "text/plain"
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun onLongClickListener(id: String): View.OnLongClickListener {
+        return View.OnLongClickListener { v: View ->
+            val item = ClipData.Item(id)
             val dragData = ClipData(
-                v.tag as? CharSequence,
+                "",
                 arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                item)
-
-            // Instantiates the drag shadow builder.
+                item
+            )
             val myShadow = TaskDragShadowBuilder(v)
 
             // Starts the drag
@@ -64,67 +61,28 @@ class TaskAdapter (
                 0           // flags (not currently used, set to 0)
             )
         }
-        holder.cardView.setOnDragListener { v, event ->
-            // Handles each of the expected events
+    }
+
+    private fun onDragListener(): View.OnDragListener {
+        return View.OnDragListener { _, event ->
             when (event.action) {
                 DragEvent.ACTION_DRAG_STARTED -> {
-                    // Determines if this View can accept the dragged data
-                    if (event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                        // As an example of what your application might do,
-                        // applies a blue color tint to the View to indicate that it can accept
-                        // data.
-                        (v as? ImageView)?.setColorFilter(Color.BLUE)
-
-                        // Invalidate the view to force a redraw in the new tint
-                        v.invalidate()
-
-                        // returns true to indicate that the View can accept the dragged data.
-                        true
-                    } else {
-                        // Returns false. During the current drag and drop operation, this View will
-                        // not receive events again until ACTION_DRAG_ENDED is sent.
-                        false
-                    }
+                    true
                 }
                 DragEvent.ACTION_DRAG_ENTERED -> {
-                    // Applies a green tint to the View. Return true; the return value is ignored.
-                    (v as? ImageView)?.setColorFilter(Color.GREEN)
-
-                    // Invalidate the view to force a redraw in the new tint
-                    v.invalidate()
                     true
                 }
 
                 DragEvent.ACTION_DRAG_LOCATION -> {
-                    // Ignore event
-
                     true
                 }
                 DragEvent.ACTION_DRAG_EXITED -> {
-                    // Re-sets the color tint to blue. Returns true; the return value is ignored.
-                    (v as? ImageView)?.setColorFilter(Color.BLUE)
-
-                    // Invalidate the view to force a redraw in the new tint
-                    v.invalidate()
                     true
                 }
-
                 DragEvent.ACTION_DRAG_ENDED -> {
-                    // Turns off any color tinting
-                    (v as? ImageView)?.clearColorFilter()
-
-                    // Invalidates the view to force a redraw
-                    v.invalidate()
-
-                    // Does a getResult(), and displays what happened.
-                    when(event.result) {
-                        true ->
-                            Toast.makeText(context, "The drop was handled.", Toast.LENGTH_SHORT)
-                        else ->
-                            Toast.makeText(context, "The drop didn't work.", Toast.LENGTH_SHORT)
-                    }.show()
-
-                    // returns true; the value is ignored.
+                    if (!event.result) {
+                        Toast.makeText(context, "The drop didn't work.", Toast.LENGTH_SHORT).show()
+                    }
                     true
                 }
                 else -> {
@@ -136,11 +94,8 @@ class TaskAdapter (
         }
     }
 
-    override fun getItemCount(): Int {
-        return tasks.count()
-    }
-
-    class TasksViewHolder(taskCellBinding: TaskCellBinding) : RecyclerView.ViewHolder(taskCellBinding.root) {
+    class TasksViewHolder(taskCellBinding: TaskCellBinding) :
+        RecyclerView.ViewHolder(taskCellBinding.root) {
         val textView: TextView = taskCellBinding.taskTitle
         val cardView: CardView = taskCellBinding.cardView
     }
