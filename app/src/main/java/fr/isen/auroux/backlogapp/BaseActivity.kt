16311ccc2d25f9784c4cd464ee4e13f4a6c945f9
axel-auroux <axel.auroux@isen.yncrea.fr>
2021-03-15 14:47:29 +0100

    update 15/03

--- app/src/main/java/fr/isen/auroux/backlogapp/BaseActivity.kt
+++ app/src/main/java/fr/isen/auroux/backlogapp/BaseActivity.kt
@@ -5,6 +5,7 @@ import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import fr.isen.auroux.backlogapp.profile.ReadAndUpdateDataActivity
 
 open class BaseActivity: AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -15,6 +16,9 @@ open class BaseActivity: AppCompatActivity() {
             Firebase.auth.signOut()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            Firebase.auth.currentUser
+            val profile = Intent(this, ReadAndUpdateDataActivity::class.java)
+            startActivity(profile)
         }
         return true
     }
